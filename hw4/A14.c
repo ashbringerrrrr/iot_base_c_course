ecc1911bfaa8e0bcf57c86d5e41c3035d62f5bd1
Igor Galkin <ashbringerrrrr@gmail.com>
2025-11-03 16:38:10 +0300

    added hw4

---
+++ hw4/A14.c
@@ -0,0 +1,42 @@
+/*
+ * A14.c
+ * 
+ * Copyright 2025 ashbringer <ashbringer@WIN-MAIN>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if no t, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	int num, a, b, c, max;
+	scanf("%d", &num);
+	
+    a = num / 100;
+    b = (num / 10) % 10;
+    c = num % 10;
+	
+	max = a > b ? a : b;
+	max = max> c ? max: c;
+	printf("%d", max);
+	return 0;
+}
+
+
---
+++ hw4/A15.c
@@ -0,0 +1,40 @@
+/*
+ * A15.c
+ * 
+ * Copyright 2025 ashbringer <ashbringer@WIN-MAIN>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if no t, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	int x1,y1,x2, y2;
+	float k,b;
+	scanf("%d%d%d%d", &x1, &y1, &x2, &y2);
+	
+	k = (float)(y2 - y1) / (x2 - x1);
+    b = y1 - k * x1;
+    
+    printf("%.2f %.2f\n", k, b);
+	return 0;
+}
+
+
