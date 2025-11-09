1593f6f344d70cf9bd74aaca0b645cb24e48df96
Igor Galkin <ashbringerrrrr@gmail.com>
2025-11-09 22:26:32 +0300

    added hw5

---
+++ hw5/B7.c
@@ -0,0 +1,51 @@
+/*
+ * B7.c
+ * 
+ * Copyright 2025 ashbringer <ashbringer@WIN-MAIN>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if no t, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	int num;
+	scanf("%d", &num);
+   
+    while (num > 0) {
+		int curr = num % 10;
+		int rem = num / 10;
+		int tmp = rem;
+        
+		while (tmp > 0) {
+			if (tmp % 10 == curr) {
+				printf("YES\n");
+				return 0;
+			}
+		tmp /= 10;
+        }
+        num = rem;
+    }
+    
+    printf("NO\n");
+    return 0;
+}
+
+
